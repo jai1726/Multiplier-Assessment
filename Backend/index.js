ff2ffac4a2a41e894e1ae2758cbebb97d77ecc1d
jai1726 <jagadeeshdende@gmail.com>
2024-10-04 15:27:06 +0530

    Functions for API calls

--- Backend/index.js
+++ Backend/index.js
@@ -24,4 +24,45 @@ mongoose.connect('MongoDb Link', {
 
 
 
+app.get('/todos', async (req, res) => {
+    try {
+        const todos = await Todo.find();
+        res.json(todos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
+app.post('/todos', async (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+    try {
+        const newTodo = await todo.save();
+        res.status(201).json(newTodo);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const todo = await Todo.findByIdAndDelete(req.params.id);
+        if (!todo) return res.status(404).json({ message: 'Todo not found' });
+        res.json({ message: 'Todo deleted' });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 
