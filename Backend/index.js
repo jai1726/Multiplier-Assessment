fa11df1d9f567d266c0d9e1860b48eb87966a14d
jai1726 <jagadeeshdende@gmail.com>
2024-10-04 15:19:14 +0530

    Schema is created

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,27 @@
-import express from "express";
-import mongoose from "mongoose"
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import Todo from './models/Todo.js';
 
-const app=express();
-app.use(express.json());
-app.use("/",router)
- 
-const PORT=5000;
-mongoose.connect("Link")
-.then(()=>app.listen(PORT))
-.then(()=>
-    console.log("connected to Database And litening to PORT"))
-.catch((err)=>console.log(err));
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+mongoose.connect('MongoDb Link', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Connection error', err);
+});
+
+
+
+
+
